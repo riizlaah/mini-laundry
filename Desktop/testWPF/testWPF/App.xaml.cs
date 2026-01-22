46e1232cc70f5940965b1599bd2ddb8a8e1cea54
Naf'an Rizkilah <100104632+riizlaah@users.noreply.github.com>
2026-01-22 14:25:03 +0700

    big change: use WPF

---
+++ Desktop/testWPF/testWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace testWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
